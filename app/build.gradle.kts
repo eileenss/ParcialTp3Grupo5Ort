f9f035c6876d6defc66985eacfac66480595144e
maticeri1 <maticeri1@gmail.com>
2024-06-01 12:26:47 -0300

    fragment navegacion drawer y diseño

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,9 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -47,7 +50,10 @@ dependencies {
     implementation(libs.retrofit)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.drawerlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
 }
--- app/src/main/java/com/example/parcialtp3grupo5ort/MainActivity.kt
+++ app/src/main/java/com/example/parcialtp3grupo5ort/MainActivity.kt
@@ -2,40 +2,42 @@ package com.example.parcialtp3grupo5ort
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.parcialtp3grupo5ort.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(){
 
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var bottomNavView: BottomNavigationView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        initViews()
+        configToolbar()
 
-        drawerLayout = findViewById(R.id.drawer_layout)
-        navigationView = findViewById(R.id.drawer_nav)
-        navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        bottomNavView = findViewById(R.id.bottom_var)
-
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true) //Mostrar icono hamburguesa
-        // Oculto el título de la Toolbar
-        supportActionBar?.setDisplayShowTitleEnabled(false)
 
         val navController = navHostFragment.navController
 
@@ -51,7 +53,26 @@ class MainActivity : AppCompatActivity() {
 
         }
         NavigationUI.setupWithNavController(bottomNavView,navHostFragment.navController)
+
+
     }
+
+    private fun configToolbar(){
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) //Mostrar icono hamburguesa
+        // Oculto el título de la Toolbar
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    private fun initViews(){
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navigationView = findViewById(R.id.drawer_nav)
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        bottomNavView = findViewById(R.id.bottom_var)
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
             drawerLayout.closeDrawer(GravityCompat.START)
@@ -61,4 +82,5 @@ class MainActivity : AppCompatActivity() {
 
         return false
     }
+
 }
